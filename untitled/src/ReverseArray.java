302cb6c9d711a78ec36b8e31a96e4deb96671c52
Rudher <rudrabhardwaj121201@gmail.com>
2024-04-08 12:32:34 +0530

    assignments

--- untitled/src/ReverseArray.java
+++ untitled/src/ReverseArray.java
@@ -8,15 +8,16 @@ public class ReverseArray {
             System.out.println(arr[i] + "");
         }
 
+
         for (i = 0, j = arr.length - 1; i < j; i++, j--) {
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
 
+        }
         System.out.println("\nReversed Array");
         for (i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + "");
-        }
         }
     }
 }
