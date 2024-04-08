e0262fc478628a090028e1fdb650d69afd257ab0
Rudher <rudrabhardwaj121201@gmail.com>
2024-04-09 00:03:47 +0530

    assignments

--- untitled/src/palindromeRecursion.java
+++ untitled/src/palindromeRecursion.java
@@ -10,7 +10,7 @@ public class palindromeRecursion {
     }
 
     public static boolean isPalindrome(String str) {
-         if (str.length() == 0 || str.length() == 1) {
+         if (str.isEmpty() || str.length() == 1) {
             return true;
         }
         if (str.charAt(0) != str.charAt(str.length() - 1)) {
